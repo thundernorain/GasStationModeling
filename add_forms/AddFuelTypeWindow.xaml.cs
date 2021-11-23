ca856135ee64c309afcaf45b2bb4e440fe2b1591
Michael <basckackov.mixail2015@yandex.ru>
2021-11-24 00:27:56 +0300

    Добавлены окна добавления в БД

---
+++ add_forms/AddFuelTypeWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GasStationModeling.add_forms
+{
+    /// <summary>
+    /// Interaction logic for AddFuelTypeWindow.xaml
+    /// </summary>
+    public partial class AddFuelTypeWindow : Window
+    {
+        public AddFuelTypeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
