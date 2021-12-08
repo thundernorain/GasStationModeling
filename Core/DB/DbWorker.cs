8ecce064db2f1401ae1aaa3e6c0378304f82b051
Sabelnikov <Artem.Sabelnikov@magenta-technology.com>
2021-12-08 08:21:18 +0400

    DB

--- Core/DB/DbWorker.cs
+++ Core/DB/DbWorker.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using GasStationModeling.core.DB.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GasStationModeling.DB
 {
-    class DbWorker
+    class DbWorker<T>: IDbWorker<T>
     {
+        private static IMongoCollection<T> collection;
+
+        private static string CollectionName;
+
+        public DbWorker(IMongoDatabase db,string collectionName)
+        {
+            collection = db.GetCollection<T>(collectionName);
+        }
+
+        public List<T> deleteEntry(int id)
+        {
+            var filter = Builders<T>.Filter.Eq("_Id", id);
+            collection.DeleteOne(filter);
+            return getCollection();
+        }
+
+        public List<T> getCollection()
+        {
+            var filter = new BsonDocument();
+            return collection.Find(filter).ToList<T>();
+        }
+
+        public T getEntry(int id)
+        {
+            var filter = Builders<T>.Filter.Eq("_Id", id);
+            return collection.Find(filter).First();
+        }
+
+        public List<T> insertEntry(T entry)
+        {
+            collection.InsertOne(entry);
+            return getCollection();
+        }
     }
 }
--- Core/DB/Interfaces/IDbWorker.cs
+++ Core/DB/Interfaces/IDbWorker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace GasStationModeling.core.DB.Interfaces
 {
-    interface IDbWorker
+    interface IDbWorker<T>
     {
+        List<T> getCollection();
+
+        T getEntry(int Id);
+
+        List<T> deleteEntry(int Id);
+
+        List<T> insertEntry(T entry);
     }
 }
